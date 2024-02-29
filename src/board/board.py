4265b99c28a13d70e2d2b9e8c9354897e7c843e7
Villalobos4113 <villalobos@fvb.one>
2024-02-29 09:49:06 -0600

    Created make_move function and cpti and ipf sub functions

--- src/board/board.py
+++ src/board/board.py
@@ -30,3 +30,38 @@ class Board:
             string_builder += "\n   +---+---+---+    +---+---+---+    +---+---+---+\n" if i != 2 else ""
         
         return string_builder
+    
+    def make_move(self, board: int, position: str) -> bool:
+        # Check if board and position are valid
+        if self.current_sub_board != -1 and self.current_sub_board != board:
+            return False
+        
+        # Convert position to index
+        position = self.convert_position_to_index(position)
+        if position == -1:
+            return False
+        
+        # Check if the position is free
+        if self.is_position_free(board, position):
+            # Make the move
+            self.sub_boards[board][position] = self.current_player
+            self.current_sub_board = position
+            self.current_player *= -1
+            return True
+        
+        return False
+    
+    def convert_position_to_index(self, position: str) -> int:
+        if len(position) != 2:
+            return -1
+        letter_value = {"A": 0, "B": 1, "C": 2, "D": 0, "E": 1, "F": 2, "G": 0, "H": 1, "I": 2}
+        column = letter_value[position[0].upper()]
+        row = (int(position[1]) - 1) % 3
+        return row * 3 + column
+    
+    def is_position_free(self, board: int, position: int) -> bool:
+        # Check if the position is in range
+        if board in range(9) and position in range(9):
+            # Check if the position is free
+            return self.sub_boards[board][position] == 0
+        raise Exception("Board and position must be in range 0-8")
