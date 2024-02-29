3f00b983ee70c698194c2f74487625ec40ad2437
Villalobos4113 <villalobos@fvb.one>
2024-02-29 10:02:12 -0600

    Fixed to_string

--- src/board/board.py
+++ src/board/board.py
@@ -22,11 +22,11 @@ class Board:
                     board[i][j] = "X" if self.main_board[i] == 1 else 'O' if self.main_board[i] == -1 else " "
         
         y_values = (0, 3, 6)
-
         string_builder = "     A   B   C        D   E   F        G   H   I\n   +---+---+---+    +---+---+---+    +---+---+---+\n"
+
         for i in range(3):
             for j in range(3):
-                string_builder += f"{i*3+j+1}  | {board[i][y_values[j]]} | {board[i][y_values[j]+1]} | {board[i][y_values[j]+2]} |    | {board[i+1][y_values[j]]} | {board[i+1][y_values[j]+1]} | {board[i+1][y_values[j]+2]} |    | {board[i+2][y_values[j]]} | {board[i+2][y_values[j]+1]} | {board[i+2][y_values[j]+2]} |\n   +---+---+---+    +---+---+---+    +---+---+---+\n"
+                string_builder += f"{i*3+j+1}  | {board[y_values[i]][y_values[j]]} | {board[y_values[i]][y_values[j]+1]} | {board[y_values[i]][y_values[j]+2]} |    | {board[y_values[i]+1][y_values[j]]} | {board[y_values[i]+1][y_values[j]+1]} | {board[y_values[i]+1][y_values[j]+2]} |    | {board[y_values[i]+2][y_values[j]]} | {board[y_values[i]+2][y_values[j]+1]} | {board[y_values[i]+2][y_values[j]+2]} |\n   +---+---+---+    +---+---+---+    +---+---+---+\n"
             string_builder += "\n   +---+---+---+    +---+---+---+    +---+---+---+\n" if i != 2 else ""
         
         return string_builder
@@ -65,3 +65,18 @@ class Board:
             # Check if the position is free
             return self.sub_boards[board][position] == 0
         raise Exception("Board and position must be in range 0-8")
+    
+    def check_win_sub(self, sb_index: int):
+        winning_combinations = [
+            (0, 1, 2), (3, 4, 5), (6, 7, 8),  # rows
+            (0, 3, 6), (1, 4, 7), (2, 5, 8),  # columns
+            (0, 4, 8), (2, 4, 6)              # diagonals
+        ]
+
+        board = self.sub_boards[sb_index]
+
+        # Check for winning combinations
+        for combo in winning_combinations:
+            if board[combo[0]] == board[combo[1]] == board[combo[2]] != 0:
+                self.main_board[sb_index] = board[combo[0]]
+                return
